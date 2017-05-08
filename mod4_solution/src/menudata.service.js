d5bc9ea3ca9ea84695a1bbff4665b5581448d6bb
hui xie <huixie01@users.noreply.github.com>
2017-05-08 15:07:11 -0400

    Add files via upload

--- mod4_solution/src/menudata.service.js
+++ mod4_solution/src/menudata.service.js
@@ -1,28 +1,26 @@
-(function () {
+(function() {
 'use strict';
 
-angular.module('Data')
-.service('MenuDataService',MenuDataService);
+angular.module('data')
+  .service('MenuDataService', MenuDataService);
 
 MenuDataService.$inject = ['$http']
-
 function MenuDataService($http) {
-    var service = this;
+  var service = this;
 
-    this.getAllCategories = function() {
-      return $http({
-          method: 'GET',
-          url: 'https://davids-restaurant.herokuapp.com/categories.json'
-        });
-    }
-    this.getItemsforCategory = function(categoryShortName){
-          return $http({
-              method: 'GET',
-              url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
-              params: {category: categoryShortName}
-          });
+  this.getAllCategories = function() {
+    return $http({
+      method: 'GET',
+      url: 'https://davids-restaurant.herokuapp.com/categories.json'
+    });
+  }
 
-    }
-
+  this.getItemsForCategory = function(categoryShortName) {
+    return $http({
+      method: 'GET',
+      url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
+      params: {category: categoryShortName}
+    });
+  }
 }
 })();
