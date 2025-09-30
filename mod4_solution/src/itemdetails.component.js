f88fddfe36d5cf4efc2658f92ea3542f52ea1e35
hui xie <26860579+huixie01@users.noreply.github.com>
2025-09-30 15:04:03 -0400

    Update itemdetails.component.js

--- mod4_solution/src/itemdetails.component.js
+++ mod4_solution/src/itemdetails.component.js
@@ -1,1 +1,21 @@
 
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemDetailsController', ItemDetailsController)
+.component('itemdetails', {
+  templateUrl: 'src/itemdetailsscomponent.template.html',
+  bindings: {
+    itemdetails: '<'
+  }
+});
+
+// 'item' is injected through state's resolve
+ItemDetailsController.$inject = ['itemdetails']
+function ItemdDetailssController(itemdetails) {
+  var itemDetailsCtrl = this;
+  this.menu_item_details = itemdetails;
+}
+
+})();
